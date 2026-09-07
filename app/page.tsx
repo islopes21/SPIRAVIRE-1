a20221c33b364ec5e2b0a39148fe19d3983c4ff4
islopes21 <is.lopes@campus.fct.unl.pt>
2026-09-07 16:25:46 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="max-w-4xl mx-auto px-6 py-16">
-      <img src="/foto.jpg" alt="A minha fotografia" width="300" />
+      <img src="public/ChatGPT Image 7_09_2026, 16_20_27.png" alt="A minha fotografia" width="300" />
       <h1 className="text-4xl font-bold mb-6">SPIRAVIRE Consortium</h1>
       <p>
         Exploring Spiro-β-lactams as Therapeutic Agents Against RNA Virus-Induced Respiratory Infections.
