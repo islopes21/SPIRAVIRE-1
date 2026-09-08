af9064eaffea9d4171328c155fd258b5ae6a29cc
islopes21 <is.lopes@campus.fct.unl.pt>
2026-09-08 11:17:25 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,15 @@ export default function Home() {
 
       <h1 className="text-4xl font-bold mb-6">SPIRAVIRE Consortium</h1>
 
-      </p>
-
       <p className="mt-4">
         The SPIRAVIRE consortium aims to develop innovative, broad-spectrum, host-directed antivirals against respiratory diseases caused by RNA viruses based on spiro-β-lactam compounds. This effort was prompted by the landmark discovery that some spiro-β-lactams have remarkable activity against diverse viruses, including HIV-1, HIV-2, influenza A and B viruses and SARS-CoV-2. The urgency for such broad-spectrum antivirals is highlighted by the need to improve global preparedness for potential epidemics caused by RNA respiratory viruses.
+      
       </p>
+      
       <h1 className="text-4xl font-bold mb-6">Get to know us!</h2>
-    
-      </p>
 
       <p className="mt-4">
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         
         <div className="p-5 border rounded-lg bg-gray-50">
           <h3 className="font-bold text-lg mb-2">Coimbra Chemistry Center (CQC)</h3>
