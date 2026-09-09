aa25f12ba81b3b7c72827283d61bb8ebb468ab7e
islopes21 <is.lopes@campus.fct.unl.pt>
2026-09-09 09:54:11 +0100

    Update page.tsx

--- app/CBR/page.tsx
+++ app/CBR/page.tsx
@@ -17,19 +17,13 @@ export default function Home() {
           marginBottom: "30px",
         }}
       >
-        Group of Organic Chemistry
+        Cell Biology of Viral Infection Laboratory
       </h1>
 
       <h2>About the group...</h2>
 
       <p style={{ lineHeight: "1.7" }}>
-        The Organic Chemistry Group at CQC is fully equipped with the
-        necessary equipment and glassware for synthetic organic chemistry
-        work. The laboratory equipment includes synthetic organic chemistry
-        equipment such as microwave reactor, flow chemistry equipment,
-        flash vacuum pyrolysis and ball mill. A variety of analytical tools
-        are available in the laboratory, including elemental analysis,
-        chiral HPLC, chiral GC, ATR-FTIR and UV/Vis spectroscopy.
+        The CBR-UCP is located at the Instituto Gulbenkian de Ciência (IGC) and benefits from world-class infrastructure through a collaboration agreement between the UCP and the Gulbenkian Foundation. Facilities of the IGC are used, under the collaboration agreement. These include rodent facility, histopathology unit and cell imaging (flow cytometry).
       </p>
 
       <h2 style={{ marginTop: "50px" }}>Members</h2>
@@ -37,54 +31,41 @@ export default function Home() {
       <div
         style={{
           display: "grid",
-          gridTemplateColumns: "repeat(3, 1fr)",
+          gridTemplateColumns: "repeat(2, 1fr)",
           gap: "30px",
           marginTop: "30px",
         }}
       >
-        {/* Teresa */}
+        {/* Marta */}
         <div style={{ textAlign: "center" }}>
           <Image
-            src="/Teresa.png"
-            alt="Teresa Pinho e Melo"
+            src="/Marta.png"
+            alt="Marta Alenquer"
             width={200}
             height={200}
             style={{ objectFit: "contain" }}
           />
 
-          <h3>Teresa Pinho e Melo, PhD, MD</h3>
+          <h3>Marta Alenquer, PhD</h3>
 
-          <p>CiênciaID: 9A1E-2767-EEE1</p>
+          <p>CiênciaID: A11D-CC4E-83E8</p>
         </div>
 
-        {/* Américo */}
+        {/* InesL */}
         <div style={{ textAlign: "center" }}>
           <Image
-            src="/Americo.png"
-            alt="Américo Alves"
+            src="/InesL.png"
+            alt="Ines Lopes"
             width={200}
             height={200}
             style={{ objectFit: "contain" }}
           />
 
-          <h3>Américo Alves, PhD, MD</h3>
+          <h3>Inês Lopes, MD</h3>
 
-          <p>CiênciaID: A018-F2DF-D0C4</p>
+          <p>CiênciaID: 191F-32DD-1901</p>
         </div>
 
-        {/* MSc researcher */}
-        <div style={{ textAlign: "center" }}>
-          <Image
-            src="/Unknown.png"
-            alt="MSc researcher"
-            width={200}
-            height={200}
-            style={{ objectFit: "contain" }}
-          />
-
-          <h3>MSc researcher</h3>
-
-          <p>To be recruited</p>
         </div>
       </div>
     </main>
