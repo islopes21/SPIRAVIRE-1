1748a1e2cc4d80c9c5d67b461f853c66aa568179
islopes21 <is.lopes@campus.fct.unl.pt>
2026-09-09 11:19:03 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout({
                   style={{
                     margin: "4px 0 0 0",
                     fontSize: "14px",
-                    color: "#666",
+                    color: "#309bae",
                   }}
                 >
                   Host-Directed Antivirals for RNA Respiratory Viruses
@@ -82,7 +82,7 @@ export default function RootLayout({
             maxWidth: "1000px",
             margin: "60px auto 0",
             padding: "40px 20px",
-            color: "#666",
+            color: "#309bae",
             fontSize: "14px",
           }}
         >
