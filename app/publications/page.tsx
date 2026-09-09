7918e9375a108a5cd687fa12dc756678cfa14b2a
islopes21 <is.lopes@campus.fct.unl.pt>
2026-09-09 15:49:35 +0100

    Update page.tsx

--- app/publications/page.tsx
+++ app/publications/page.tsx
@@ -2,7 +2,19 @@ export default function Publications() {
   return (
     <main className="max-w-4xl mx-auto px-6 py-16 space-y-6">
       <h1 className="text-3xl font-semibold mb-6">Publications</h1>
-      <p>No publications yet. This page will list SPIRAVIRE consortium papers and references.</p>
+      <p>No publications yet. </p>
+      <p>Please read the Related bibliography to learn more!</p>
+      <h1 className="text-3xl font-semibold mb-6">Related Bibliography</h1>
+      <p>1.  Alves, A. J. S. et al. Unveiling a family of spiro-β-lactams with anti-HIV and antiplasmodial activity via phosphine-catalyzed [3+2] annulation of 6-alkylidene-penicillanates and allenoates. Front. Chem. 10, 1017250 (2022).
+</p>
+      <p>2.  Alves, N. G. et al. Synthesis and structure-activity relationships of new chiral spiro-β-lactams highly active against HIV-1 and Plasmodium. European Journal of Medicinal Chemistry 219, 113439 (2021).
+</p>
+      <p>3.  Bártolo, I. et al. Spiro-β-lactam BSS-730A Displays Potent Activity against HIV and Plasmodium. ACS Infect. Dis. 7, 421–434 (2021).
+</p>
+      <p>4.  Alves, A. J. S. et al. Spiro-Lactams as Novel Antimicrobial Agents. CTMC 20, 140–152 (2020).
+</p>
+      <p>5.  Bártolo, I. et al. High Instantaneous Inhibitory Potential of Bictegravir and the New Spiro-β-Lactam BSS-730A for HIV-2 Isolates from RAL-Naïve and RAL-Failing Patients. IJMS 23, 14300 (2022).
+ </p>
     </main>
   );
 }
