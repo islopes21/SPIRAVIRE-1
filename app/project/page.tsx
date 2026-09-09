e6b5a8fb362f14356f131165649e666c2e517ea8
islopes21 <is.lopes@campus.fct.unl.pt>
2026-09-09 14:50:31 +0100

    Update page.tsx

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,1 +1,17 @@
+export default function ProjectPage() {
+  return (
+    <main
+      style={{
+        maxWidth: "1600px",
+        margin: "0 auto",
+        padding: "50px 40px",
+      }}
+    >
+      <h1>Project Overview</h1>
 
+      <p>
+        Welcome to the SPIRAVIRE Consortium project.
+      </p>
+    </main>
+  );
+}
